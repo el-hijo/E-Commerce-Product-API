6da72409a72ac7719820ba4a2873dcaf751371ec
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-09 15:44:56 +0100

    created inventory viewset in views.py

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Inventory
+from .serializers import InventorySerializer
 
 # Create your views here.
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
