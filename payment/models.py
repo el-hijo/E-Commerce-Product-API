1870cf1be6e0a382824c82e0cc2484fda192b881
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-14 16:30:01 +0100

    created model in models.py for payment app

---
+++ payment/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from orders.models import Order
+# Create your models here.
+
+class Payment(models.Model):
+    STATUS_CHOICES = [("PENDING", "Pending"),("SUCCESS", "Success"),("FAILED", "Failed"),]
+
+    order = models.OneToOneField(Order,on_delete=models.CASCADE,related_name="payment")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default="PENDING")
+    payment_method = models.CharField(max_length=50) 
+    transaction_ref = models.CharField(max_length=255, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Payment {self.transaction_ref} - {self.status}"
