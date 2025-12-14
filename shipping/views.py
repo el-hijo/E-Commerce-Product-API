fd2779831f49cd56a5b5e284b8a98779dbe73065
Akintunde Chugbo Adeosun <reverencechugbo@yahoo.co.uk>
2025-12-14 10:33:42 +0100

    created shipping/views.py

--- shipping/views.py
+++ shipping/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import ShippingAddress
+from .serializers import ShippingAddressSerializer
 
 # Create your views here.
+
+class ShippingAddressViewSet(viewsets.ModelViewSet):
+    serializer_class = ShippingAddressSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return ShippingAddress.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
